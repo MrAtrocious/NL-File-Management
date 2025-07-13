d333e68d6504999c1814fb1bdd77dee008e6c7e7
itsjust._.ted <tedolivahs@gmail.com>
2025-07-13 18:36:31 -0400

    Update .jules-setup.sh

--- .jules-setup.sh
+++ .jules-setup.sh
@@ -9,3 +9,7 @@ npm install jest fs-extra --save-dev
 
 # Set file permissions (if needed)
 chmod +x scripts/*.sh
+
+#!/bin/bash
+npm install
+npm install -D tailwindcss @tailwindcss/forms @tailwindcss/typography
